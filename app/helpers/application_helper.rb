b78f68a665e1e7b1ca8b113bb45d5082448cd949
Aditya-Kansara <aditya.k@simformsolutions.com>
2023-03-13 17:33:55 +0530

    feat: TE7-T118 Active Record Validations
    created two models Student and Faculty and added validations as suggested
    created CRUD for both Models and displayed proper validation error messages
    optimized the code as much as possible

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def birthdate_cannot_be_in_the_future
+    return unless birthdate.present? && birthdate > Date.today
+    errors.add(:birthdate, "can't be in the future")
+  end
 end
---
+++ app/helpers/faculties_helper.rb
@@ -0,0 +1,4 @@
+module FacultiesHelper
+  DESIGNATION = { 'Select Designation': 'nil', 'Assistant Professor': 'Ass. Prof.', 'Professor': 'Prof.',
+                  'Head of Department': 'HOD', 'Senior Professor': 'Sr. Prof.' }.freeze
+end
